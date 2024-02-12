0f8f14399e1acdb7b8951aa6f027949e9ae8b81a
Michael Green <github@ftt6283.org>
2024-02-12 19:49:39 +0000

    fix path for rerun

--- Fiji_ER_Estimate_Values.R
+++ Fiji_ER_Estimate_Values.R
@@ -12,7 +12,7 @@ library(FijiNFMSCalculations)
 
 
 getDataPath<-function(filename) {
-  return(paste0("./Data/mrUpdateOct23/", filename))
+  return(paste0("./Data/mrAuditFeb24/", filename))
 }
 
 load(file = getDataPath("Fiji_ER_Estimate_AccuracyAssessment.RData"))
